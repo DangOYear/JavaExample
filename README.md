c82ec5bcf9cf56375d30f557ed9e4fca6d6cbf9e
ShengyunYu <yushengyun2013@163.com>
2021-01-10 17:11:49 +0800

    Java delete the stream package under lambda

--- README.md
+++ README.md
@@ -13,4 +13,3 @@
         + Guava
         + Lombok
         + Netty
-       
--- src/main/java/org/saltedfish/lambda/stream/StreamDemo.java
+++
@@ -1,7 +0,0 @@
-package org.saltedfish.lambda.stream;
-
-public class StreamDemo {
-    public static void main(String[] args) {
-
-    }
-}
